de97888ea624f9e8649a2441d74cf605bdf11f03
github Toshiaki Takagi <maris160zhk@gmail.com>
2023-09-09 14:07:34 +0900

    renderTest

--- app/src/Render.test.tsx
+++ app/src/Render.test.tsx
@@ -4,7 +4,13 @@ import { Render } from "./Render";
 describe("Rendering", () => {
   it("全ての要素がレンダリングされているか", () => {
     render(<Render />);
-    //screen.debug();
-    screen.debug(screen.getByRole("heading"));
+    expect(screen.getByRole("heading")).toBeTruthy(); //要素が存在するか
+    expect(screen.getByRole("textbox")).toBeTruthy();
+    expect(screen.getAllByRole("button")[0]).toBeTruthy(); //１つ目のボタン
+    expect(screen.getAllByRole("button")[1]).toBeTruthy();
+    expect(screen.getByText("Udemy")).toBeTruthy(); //文字列が存在するか
+    expect(screen.getByText("@React")).toBeTruthy();
+    expect(screen.queryByText("neko")).toBeNull(); //存在しないテキスト判定
+    expect(screen.getByTestId("test20230909")).toBeTruthy();
   });
 });

41dffbbef73b6c6b132142361b59639c55eaa71a
github Toshiaki Takagi <maris160zhk@gmail.com>
2023-09-25 22:32:21 +0900

    テスト用コード作成

--- app/src/ReduxAsync.tsx
+++ app/src/ReduxAsync.tsx
@@ -1,15 +1,23 @@
 import { useSelector } from "react-redux";
-import { selectCount, fetchDummy } from "./cutomCounter/customCounterSlice";
+import {
+  selectCount,
+  selectUsername,
+  fetchDummy,
+  fetchJSON,
+} from "./cutomCounter/customCounterSlice";
 import { useAppDispatch } from "./cutomCounter/store";
 
 const ReduxAsync = () => {
   const count = useSelector(selectCount);
+  const username = useSelector(selectUsername);
   const dispatch = useAppDispatch();
 
   return (
     <div>
       <span data-testid="count-value">{count}</span>
       <button onClick={() => dispatch(fetchDummy(5))}>FetchDummy</button>
+      {username && <h1>{username}</h1>}
+      <button onClick={() => dispatch(fetchJSON())}>FetchJSON</button>
     </div>
   );
 };
